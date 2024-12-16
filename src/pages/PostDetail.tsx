ace85d8e1b3a5c63c132369bed80b2742912a770
songwon-sung <swsung91@gmail.com>
2024-12-16 16:58:48 +0900

    fix: 확인용 콘솔로그 주석처리리

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -50,7 +50,7 @@ export default function PostDetail() {
       startLoading();
       // 여기에 포스트 id 값 넣기
       const { data } = await api.get(`/posts/${post_id}`);
-      console.log(data);
+      // console.log(data);
 
       const {
         author: { fullName, _id: userID, image: userImg },
